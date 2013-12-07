e366712ad3b57c3d444ea526a33dd74ae8a77ad7
Matt Hardwick <matthew.hardwick@gmail.com>
2013-12-07 12:47:26 -0800

    more testing

--- hello.py
+++ hello.py
@@ -1,11 +1,17 @@
 import os
-from flask import Flask
-#import flask
+from flask import Flask,jsonify,render_template
 
 app = Flask(__name__)
+file = None
 
 @app.route('/')
+def index():
+    file = open('inputfile1.txt').readlines()
+    return render_template('index.html', input_file=file)
+
+@app.route('/json')
 def hello():
-    someDict = { thisKey: value }
-    #return flask.jsonify(someDict)
-    return "hello"
+    someDict = { 'thisKey': 'value' }
+    if file is None:
+        file = []
+    return jsonify(results = file)
