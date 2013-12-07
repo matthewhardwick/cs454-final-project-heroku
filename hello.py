50fd624700b9da55c3178046fee94ec1fbd898ac
Matt Hardwick <matthew.hardwick@gmail.com>
2013-12-07 02:47:46 -0800

    testing

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
+#import flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'hello World!'
+    someDict = { thisKey: value }
+    #return flask.jsonify(someDict)
+    return "hello"
